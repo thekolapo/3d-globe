6f3233cac59424bdaaf7fc9c6c19a2e4408e98aa
Kolapo Oni <onikolapo@gmail.com>
2021-10-21 21:13:58 +0100

    initialize 3d scene and add a sphere

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,61 @@
 <template>
-  <div class="c-app"></div>
+  <div ref="container" class="c-app"></div>
 </template>
 
 <script>
+import * as THREE from 'three'
+// let renderer, scene, camera
+
 export default {
   components: {},
   data() {
     return {}
   },
-  mounted() {},
-  methods: {},
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const scene = new THREE.Scene()
+      const camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      )
+
+      camera.position.set(0, 0, 20)
+
+      const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+      // renderer.setClearColor('bisque', 0.5)
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      this.$refs.container.appendChild(renderer.domElement)
+
+      const geometry = new THREE.SphereGeometry(5, 20, 50)
+      // const material = new THREE.MeshBasicMaterial({ color: '#232225' })
+      const material = new THREE.MeshPhongMaterial({
+        color: '#33334c',
+        opacity: 1,
+      })
+      material.transparent = true
+
+      const sphere = new THREE.Mesh(geometry, material)
+
+      const ambientLight = new THREE.AmbientLight(0x888888)
+      // Create a new directional light
+      const directionalLight = new THREE.DirectionalLight(0xfdfcf0, 1)
+      directionalLight.position.set(20, 10, 20)
+      scene.add(directionalLight)
+      scene.add(ambientLight)
+      scene.add(sphere)
+
+      const animate = () => {
+        requestAnimationFrame(animate)
+        renderer.render(scene, camera)
+      }
+      animate()
+    },
+  },
 }
 </script>
 
@@ -20,6 +66,6 @@ export default {
   align-items: center;
   width: 100%;
   min-height: 100vh;
-  // background: bisque;
+  background: #33334c;
 }
 </style>
